6ceaea8afdcb1e53a49a0890a0d04be59a5f0508
jerardbarca <45223726+jerardbarca@users.noreply.github.com>
2019-02-20 15:15:11 +0800

    event registration

---
+++ event.php
@@ -0,0 +1,33 @@
+
+<?php
+	echo $FirstName: ; echo $_POST["intusername"];
+	
+	echo "<br>";
+	
+	echo $LastName: ; echo $_POST["intusername2"];
+
+	echo "<br>";
+	
+	echo $Nickname:	; echo $_POST ["intNickname"]
+	
+	echo "<br>";
+	
+	echo $Email:; echo $_POST ["intEmail"];
+
+	
+	echo "<br>";
+	
+	echo $Option:;	echo $_POST ["Option"];
+	
+	echo "<br>";
+	
+	echo $areacode: ;	echo $_POST ["areacode"];
+
+	echo $Number:; echo $_POST ["intNumber"];
+	
+	echo "<br>";
+	
+	echo $Choice: 	; echo $_POST ["Choice"];
+
+
+?>
